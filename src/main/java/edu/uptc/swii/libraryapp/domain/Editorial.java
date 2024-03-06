96649cde6ca9c0f8d8aba71e6c8b86b94751f8de
Sebastian Palacios <sebpal005@gmail.com>
2024-03-06 16:59:33 -0500

    add editorial domain

---
+++ src/main/java/edu/uptc/swii/libraryapp/domain/Editorial.java
@@ -0,0 +1,41 @@
+package edu.uptc.swii.libraryapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Editorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String webSite;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public void setWebSite(String webSite) {
+        this.webSite = webSite;
+    }
+
+}
